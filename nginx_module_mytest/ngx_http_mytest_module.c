561f38eacd9b855bcf423977596e6354b85ad3c6
yejianfeng <jianfengye110@gmail.com>
2013-06-10 16:47:11 +0800

    http_mytest_module finish
    http_mytest_module finish

--- nginx_module_mytest/ngx_http_mytest_module.c
+++ nginx_module_mytest/ngx_http_mytest_module.c
@@ -1,27 +1,27 @@
 
+/*
+ * Copyright (C) yejianfeng
+ */
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r);
+static char * 
+ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
 static ngx_command_t ngx_http_mytest_commands[] = {
     {
         ngx_string("mytest"),
         NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LMT_CONF|NGX_CONF_NOARGS,
-        ng_http_mytest,
+        ngx_http_mytest,
         NGX_HTTP_LOC_CONF_OFFSET,
         0,
         NULL
     },
     ngx_null_command
 };
-
-static char * 
-ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
-{
-    ngx_http_core_loc_conf_t *clcf;
-
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-
-    clcf->handler = ngx_http_mytest_handler;
-
-    return NGX_CONF_OK;
-}
 
 static ngx_http_module_t ngx_http_mytest_module_ctx = {
     NULL,
@@ -30,8 +30,9 @@ static ngx_http_module_t ngx_http_mytest_module_ctx = {
     NULL,
     NULL,
     NULL,
-    ngx_http
-}
+    NULL,
+    NULL
+};
 
 ngx_module_t ngx_http_mytest_module = {
     NGX_MODULE_V1,
@@ -48,13 +49,27 @@ ngx_module_t ngx_http_mytest_module = {
     NGX_MODULE_V1_PADDING
 };
 
-static ngx_init_t ngx_http_mytest_handler(ngx_http_request_t *r)
+
+static char * 
+ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+
+    clcf->handler = ngx_http_mytest_handler;
+
+    return NGX_CONF_OK;
+}
+
+
+static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 {
     if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
         return NGX_HTTP_NOT_ALLOWED;
     }
 
-    ngx_init_t rc = ngx_http_discard_request_body(r);
+    ngx_int_t rc = ngx_http_discard_request_body(r);
     if (rc != NGX_OK) {
         return rc;
     }
